f8d159b22d252672b5534e4d4129d6208dafce8a
Maria del Mar Deyà <145254398+mardeya@users.noreply.github.com>
2025-05-19 23:56:33 +0200

    Update Patient_dashboard.py

--- Patient_dashboard.py
+++ Patient_dashboard.py
@@ -11,7 +11,6 @@ import dalex as dx
 from sklearn.impute import KNNImputer
 from lime.lime_tabular import LimeTabularExplainer
 
-
 selected_variables = [
     'Age',
     'Gender', 
@@ -40,65 +39,67 @@ def show_dashboard():
         gender = st.segmented_control("Sex", ["M", "F"])
         gender = 0 if gender == "M" else 1
     with col2:
+        obesity = st.radio('Have you obesity?', ["No", "Yes"])
+        obesity = 1 if obesity == "Yes" else 0
+    with col3:
+        val = st.radio('Previous Heart Problems', ["No", "Yes"])
+        val = 1 if val == "Yes" else 0
+    with col4:
+        diabetes = st.radio("Do you have diabetes?",("No", "Yes"))
+        diabetes = 1 if diabetes == "Yes" else 0
+       
+
+    colx, col5, col6, col7 = st.columns(4)
+    with colx:
+        alcohol = st.radio("Do you drink alcohol?",("No", "Yes"))
+        alcohol = 1 if alcohol == "Yes" else 0
+    with col5:
+        medication = st.radio("Do you take any medication?",("No", "Yes"))
+        medication = 1 if medication == "Yes" else 0
+    with col6:
+        smoking = st.radio("Do you smoke?",("No", "Yes"))
+        smoking = 1 if smoking == "Yes" else 0
+    with col7:
+        family_history = st.radio("Do you have any family history of heart attacks?",("No", "Yes"))
+        family_history = 1 if family_history == "Yes" else 0
+
+    col8, col9, col10= st.columns(3)
+    with col8:
+        diet = st.radio("How is your diet?", ("Unhealthy", "Normal", "Healthy"))
+        diet_map = {"Unhealthy": 0, "Normal": 1, "Healthy": 2}
+        diet = diet_map.get(diet, -1)
+    with col9:
         age = st.number_input(
             label="Introduce your age",
             min_value=18,
             max_value=90
         )
-    with col3:
-        diabetes = st.selectbox("Do you have diabetes?",("No", "Yes"))
-    with col4:
-        family_history = st.selectbox("Do you have any family history of heart attacks?",("No", "Yes"))
-
-       
-
-    colx, col5, col6, col7 = st.columns(4)
-    with colx:
-        alcohol = st.selectbox("Do you drink alcohol?",("No", "Yes"))
-
-    with col5:
-        medication = st.selectbox("Do you take any medication?",("No", "Yes"))
-
-    with col6:
-        smoking = st.selectbox("Do you smoke?",("No", "Yes"))
-    with col7: 
-        diet = st.selectbox("How is your diet?",("Unhealthy", "Normal", "Healthy"))    
-        diet_map = {"Unhealthy": 0, "Normal": 1, "Healthy": 2}
-        diet = diet_map.get(diet, -1) 
-
-    col8, col9, col10= st.columns(3)
-    with col8:
-        exercise = st.number_input(
-            label="How many hours a week do you exercise?",
-            min_value=0,
-            max_value=20
-        )
-    with col9:
+    with col10:
         sleep = st.number_input(
             label="How many hours do you sleep per day?",
             min_value=0,
             max_value=12
         )
-    with col10:
+
+    col11, col12, col13 = st.columns(3)
+    with col11:
         sedentary = st.number_input(
             label="How many hours do you sit?",
             min_value=0,
             max_value=12
         )
-
-    col11, col12, col13 = st.columns(3)
-    with col11: 
-        obesity = st.radio('Have you obesity?', ["Yes", "No"])
-        obesity = 1 if obesity == "Yes" else 0
     with col12: 
+        exercise = st.number_input(
+            label="How many hours a week do you exercise?",
+            min_value=0,
+            max_value=20
+        )
+    with col13:
         stress = st.number_input(
             label="From 1 to 10, which is your strees level?",
             min_value=1,
             max_value=10
         )
-    with col13: 
-        val = st.radio('Previous Heart Problems', ["Yes", "No"])
-        val = 1 if val == "Yes" else 0
 
     _, col14, _= st.columns(3) 
     with col14:
@@ -116,12 +117,9 @@ def show_dashboard():
             emoji = "💪"
 
         st.markdown(f"###  You have {emoji} **{decision_text}** of having a heart attack.")        
-        if risk_percent < 30:
+        if risk_percent < 50:
             color = "green"
             image_path = "images/SemaforoVerde.png"
-        elif 30 <= risk_percent < 70:
-            color = "yellow"
-            image_path = "images/SemaforoAmarillo.png"
         else:
             color = "red"
             image_path = "images/SemaforoRojo.png"
@@ -158,7 +156,7 @@ def show_dashboard():
         local_exp = explainer.predict_parts(pred)
 
         # Explanation Breakdown Plot
-        st.header(f"Breakdown Explanation for your data")
+        st.header(f"Breakdown Explanation for Your Risk Profile")
         fig = local_exp.plot(show=False)  
         st.plotly_chart(fig)
 
@@ -170,7 +168,7 @@ def show_dashboard():
         menores_contribuciones = top_features[top_features['contribution'] < 0].tail(5)
 
         # Generar explicación en texto
-        st.subheader("Explanation in words:")
+        st.subheader("Top Risk-Increasing and Risk-Reducing Factors:")
         explicacion = "Based on your data, the factors that increase your heart attack risk the most are:\n"
         for _, row in mayores_contribuciones.iterrows():
             if ((row['variable'] != 'prediction') & (row['variable'] != 'intercept')):
@@ -181,7 +179,6 @@ def show_dashboard():
             explicacion += f"- **{row['variable']}** (contribution: {row['contribution']:.2f})\n"
 
         st.markdown(explicacion)
-
 
         X_train = explain_dashboard().values
 
@@ -202,14 +199,12 @@ def show_dashboard():
         )
 
         # Mostrar la explicación como texto y gráfico
-        st.subheader("Explanation of the prediction with LIME")
+        st.subheader("Personalized visual explanation of your result")
         st.pyplot(explanation.as_pyplot_figure())
 
-        st.subheader("LIME feature contributions (text)")
+        st.subheader("Factors that influenced your result the most")
         for feature, weight in explanation.as_list():
             st.markdown(f"- **{feature}**: {weight:+.2f}")
-
-        
 
 def preprocess_input(pred):
     new_pred = []
